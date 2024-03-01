11c888517f4be836435be197bb0b0536b2fd666d
EL-Suescun <santiagogiraldosuescun@gmail.com>
2024-03-01 07:13:39 -0500

    Creación archivo Ejercicio 3

--- Ejercicio 3/Program.cs
+++ Ejercicio 3/Program.cs
@@ -1,1 +1,9 @@
-﻿/*  */
+﻿/* El programa tomará la edad del usuario y determinará cuántos días y semanas han transcurrido desde el año actual. */
+
+Console.Write("Ingrese su edad: ");
+int edad = int.Parse(Console.ReadLine());
+
+// Obtener el año actual
+int año_actual = DateTime.Now.Year;
+
+int año_nacimiento 
