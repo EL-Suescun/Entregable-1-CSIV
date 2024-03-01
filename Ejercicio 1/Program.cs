cd9fd09738021f042a0b939556ad6102201aa896
EL-Suescun <santiagogiraldosuescun@gmail.com>
2024-03-01 06:32:53 -0500

    Ejercicio 1 terminado
    Se termina el resto de funciones de la calculadora

--- Ejercicio 1/Program.cs
+++ Ejercicio 1/Program.cs
@@ -1,7 +1,7 @@
 ﻿/* Crear una calculadora que maneje fracciones. El usuario ingresará dos 
 fraciones y un operador. La calculadora deberá realizar la operación. Debes 
 manejar sumas, restas, multiplicaciones y divisiones. Si el usuario intenta
-dividir entre deberás mostrar un mensaje de error */
+dividir entre cero deberás mostrar un mensaje de error */
 
 Console.Write("Ingrese el primer numero: ");
 float fraccion_1 = float.Parse(Console.ReadLine());
@@ -24,13 +24,20 @@ switch (operacion)
         Console.Write($"La suma de {fraccion_1} y {fraccion_2}, es: {suma}");
         break;
     case 2:
-        
+        float resta = fraccion_1 - fraccion_2;
+        Console.Write($"La resta de {fraccion_1} y {fraccion_2}, es: {resta}");
         break;
     case 3:
-        
+        float multiplicacion = fraccion_1 * fraccion_2;
+        Console.Write($"La multiplicacion de {fraccion_1} por {fraccion_2}, es: {multiplicacion}");
         break;
     case 4:
-        
+        if (fraccion_2 == 0){
+           Console.Write($"Error: \n No se puede dividir por cero"); 
+        }else{
+           float division = fraccion_1 / fraccion_2;
+        Console.Write($"La division de {fraccion_1} por {fraccion_2}, es: {division}"); 
+        }
         break;    
     default:
         Console.WriteLine("Opcion no valída.");
