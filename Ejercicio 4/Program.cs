d9d8b4a9c5ecd3592f804e51eea85c7a870f18b1
EL-Suescun <santiagogiraldosuescun@gmail.com>
2024-03-01 07:29:17 -0500

    Creación del archivo del Ejercicio 4
    Se creo la carpeta que contiene el Ejercicio 4, a pesar de no haber terminado aun el Ejercicio 3, pues parece que es más dificil que el Ejercicio 4

---
+++ Ejercicio 4/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Implementar un programa que simule un sistema de incio de sesiòn. El usuario debe ingresar su nombre de usuario y contraseña. Si el nombre de usuario y la contraseña coincide con los valores predefinidos, el usuario puede acceder al sistema. Si no, se le debe informar que el nombre de usuario o contrañse son incorrectos y se debe permitir intentarlo nuevamente, hasta un máximo de 3 intentos.  
+
+Instrucciones: 
+° Define un nombre de usuario y una contraseña predefinidos en tu código. 
+° Solicita al usuario que ingrese su nombre de usuario y contraseña.
+° Compara los valores ingresados por el usuario con los valores predefinidos.
+° Si el nombre de usuario y la contraseña coinciden con los valores predefinidos muestra un mensaje que diga "Acceso concedido" y finaliza el programa 
+° Si no coinciden, informa al usuario que el nombre de usuario o la contraseña son incorrectos y reduce en uno el contador de intentos
+° Si el usuario agota sus 3 intentos, muestra un mensaje que diga "Acceso denegado" y finaliza el programa 
+° Asegúrate de proporcionar retroalimentación al usuario después de cada intento, indica cuántos intentos quedan*/
+
+
+
