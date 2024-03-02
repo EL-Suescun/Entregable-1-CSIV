408a4d48a962a8047bede4440e96d828d7945ae6
EL-Suescun <santiagogiraldosuescun@gmail.com>
2024-03-01 20:59:12 -0500

    Ejercicio 3
    Se termino el ejercico número 3 ATT: Bovea

--- Ejercicio 3/Program.cs
+++ Ejercicio 3/Program.cs
@@ -1,9 +1,31 @@
 ﻿/* El programa tomará la edad del usuario y determinará cuántos días y semanas han transcurrido desde el año actual. */
-
-Console.Write("Ingrese su edad: ");
+/* Ingresar edad */
+Console.WriteLine("Ingrese su edad");
 int edad = int.Parse(Console.ReadLine());
 
-// Obtener el año actual
-int año_actual = DateTime.Now.Year;
+String fecha = DateTime.Now.AddYears(0).ToString("yyyy");
+int fechaFinal = int.Parse(fecha);
 
-int año_nacimiento 
+/* FechaInicial */
+int fechaInicial = fechaFinal-edad;
+
+/* Determinar cantidad de bisiestos */
+int cntidadBisiestos=0;
+for (int i = fechaInicial; i <= fechaFinal; i++)
+{
+    if (i%4==0)
+    {
+       cntidadBisiestos+=1;
+    }
+}
+
+Console.WriteLine("bisiestos: " + cntidadBisiestos);
+
+/* determinar dias y semanas */
+int cantidadNoBisiestos = edad-cntidadBisiestos;
+int dias = (cantidadNoBisiestos*365)+(cntidadBisiestos*366);
+int semanas = dias/7;
+
+/* output */
+Console.WriteLine("dias transcurridos: "+dias);
+Console.WriteLine("semanas transcurridas: "+semanas);
